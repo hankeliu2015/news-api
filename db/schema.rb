b45dd599d4c960e4e37f2f5fc6beec8d4cfcbbb2
github username <github email address>
2019-08-08 21:43:06 -0400

    remove score column from stories table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_173556) do
+ActiveRecord::Schema.define(version: 2019_08_09_014047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_08_08_173556) do
 
   create_table "stories", force: :cascade do |t|
     t.string "comment"
-    t.integer "score"
     t.integer "story_id"
     t.string "image"
     t.string "resume"
